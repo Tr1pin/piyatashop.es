8388e713b0009b49eb372f778dfd0de6ada01b2f
Denis <camblorcaberodenis@gmail.com>
2025-08-30 22:06:33 +0200

    Update talwind.config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    safelist: [
+    'animate-slide-in-bottom-shirt',
+    'animate-slide-in-bottom-bag',
+    'animate-slide-in-bottom-mug',
+    'animate-slide-in-bottom-ball',
+    'animate-slide-in-bottom-ballBeige',
+    'animate-slide-in-bottom-belt',
+    'animate-slide-in-bottom-harness',
+  ],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
